f10a4e160440baa77a10afd201682121878f6069
Connor Ketcheson <connorketcheson01@gmail.com>
2022-12-02 09:38:33 +0900

    start factory, and create turn system

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,91 +1,83 @@
-//create an array to store the gameboard using a module; [x,x,x;x,x,x;,x,x,x]
+const createPlayer = (sign) => {
+    const getSign = () => sign;
+    const turnsPlayed = () => 0;
+    const nextTurn = turnsPlayed => {
+        turnsPlayed += 1;
+    }
+    
+    return {getSign, turnsPlayed, nextTurn}
+};
 
-// let gameBoard = [x,o,x,
-//                  o,x,o,
-//                  o,x,o];
-
-//create players using a factory function
-
-// const createPlayer = (sign) => {
-//     const xButton = document.getElementById("xButton");
-//     const oButton = document.getElementById("oButton");
-//     const getSign = () => {
-//         xButton.addEventListener("click", () => {
-//             if (player1 = undefined) {
-//                 sign = x;
-//             } else {
-//                 alert("You have already selected your signs!");
-//             }
-//         });
-//         oButton.addEventListener("click", () => {
-//             if (player1 = undefined) {
-//                 sign = o;
-//             } else {
-//                 alert("You have already selected your signs!");
-//             }
-//         });
-
-//     }
-//     return{getSign};
-// };
-//   //add button id as sign in factory function
-
-// const player1 = createPlayer("player 1", xButton.innerHTML);
-// const player2 = createPlayer("player 2", o);
-
-const createPlayer = {
-    const playerSign = () => 
-}
-
-// const player1 = createPlayer(player1, x)
-
-// const player2 = Player(player2, o)
-
-//create a function to draw the game board onto the page usine DOM
-
-
+const player1 = createPlayer("x");
+const player2 = createPlayer("o");
+console.log(player1);
+console.log(player2);
 
 const makeBoard = (() => {
     const board = document.getElementById("board");
     const makeColumns = () => {
-        console.log("test");
         for (i = 0; i < 3; i++) {
             let column = document.createElement("div");
             board.appendChild(column).className = "boardColumn";
+            column.id = `column${i}`;
         }
     };
 
     const column = document.getElementsByClassName("boardColumn");
 
+    let turns = 0;
     const makeTiles = () => {
         for (j = 0; j < 3; j++) {  
             for (k = 0; k < 3; k++) {
                 let tile = document.createElement("tile");
                 column[k].appendChild(tile).className = "tile";
                 tile.addEventListener("click", () => {
-                    console.log("test");
-                    tile.innerHTML = "x"; 
+                    if (turns % 2 == 0 && tile.innerHTML == "") {
+                        tile.innerHTML = "x"; 
+                        player1.nextTurn;
+                        turns += 1;
+                    } else if (turns % 2 !== 0 && tile.innerHTML == "") {
+                        tile.innerHTML = "o"; 
+                        player2.nextTurn;
+                        turns += 1;
+                    }
                 });
             }
         }
     };
 
+    const checkTurns = () => {
+        if (turns == 3) {
+            for (i = 0; i < tile.length; i++) {
+                tile[i].innerHTML = "";
+            }
+        }
+    };
+    
+    //const checkWin
+
     return {
         makeColumns,
         makeTiles,
+        checkTurns
     };
 })();
 
 makeBoard.makeColumns();
 makeBoard.makeTiles();
+makeBoard.checkTurns();
 
 
-    //create a function to mark a tile using mouseDown event
-    //players cannot mark the same tile twice; if (tile is occupied)...
+// (column0[i] = column1[i] = column2[i])
 
-//create a condition to tell if a player has won
-    //this can be a function which is called every time a player selects a tile;
-    //the function will check if there are 3 same in a row, if not, nothing will happen
-    //additionally, when there are 3 in a row, launch a message
+// (column0[0] = column1[1] = column2[2])
 
-//create a button to restart game
+// (column0[2] = column1[1] = column2[0])
+
+// (column0[0] = column0[1] = column0[2])
+
+// (column1[0] = column1[1] = column1[2])
+
+// (column2[0] = column2[1] = column2[2])
+
+
