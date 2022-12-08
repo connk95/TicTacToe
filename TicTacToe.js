16ceb6cbce54d3305f2df7dfb09825917d5be2e8
Connor Ketcheson <connorketcheson01@gmail.com>
2022-12-08 16:44:01 +0900

    change to static grid

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,71 +1,67 @@
-const createPlayer = (sign) => {
-    const getSign = () => sign;
-    const turnsPlayed = () => 0;
-    const nextTurn = turnsPlayed => {
-        turnsPlayed += 1;
-    }
-    
-    return {getSign, turnsPlayed, nextTurn}
-};
-
-const player1 = createPlayer("x");
-const player2 = createPlayer("o");
-console.log(player1);
-console.log(player2);
-
 const makeBoard = (() => {
-    const board = document.getElementById("board");
-    const makeColumns = () => {
-        for (i = 0; i < 3; i++) {
-            let column = document.createElement("div");
-            board.appendChild(column).className = "boardColumn";
-            column.id = `column${i}`;
-        }
-    };
 
-    const column = document.getElementsByClassName("boardColumn");
+    const createPlayer = (player, sign, turn, ai) => {
+        // const playerNumber = () => player
+        // const getSign = () => sign;
+        // const myTurn = () => turn;
+        // const isAI = () => ai
+        
+        return {player, sign, turn, ai}
+    };
+    
+    const player1 = createPlayer("1", "x", "true", "false");
+    const player2 = createPlayer("2", "o", "false", "false");
+    console.log(player1);
+    console.log(player2);
 
     let turns = 0;
-    const makeTiles = () => {
-        for (j = 0; j < 3; j++) {  
-            for (k = 0; k < 3; k++) {
-                let tile = document.createElement("tile");
-                column[k].appendChild(tile).className = "tile";
-                tile.addEventListener("click", () => {
-                    if (turns % 2 == 0 && tile.innerHTML == "") {
-                        tile.innerHTML = "x"; 
-                        player1.nextTurn;
-                        turns += 1;
-                    } else if (turns % 2 !== 0 && tile.innerHTML == "") {
-                        tile.innerHTML = "o"; 
-                        player2.nextTurn;
-                        turns += 1;
-                    }
-                });
-            }
-        }
-    };
+    const tiles = document.getElementsByClassName("tile"); 
+    for (i = 0; i < tiles.length; i++) {  
+    tiles[i].addEventListener("click", () => {
+        
+        console.log(tiles[i]);
+        if (player1.turn == "true" && this.className == "tile") {
+            tiles[i].className = "x"; 
+            player1.turn = "false";
+            player2.turn = "true";
+            turns += 1;
+        } else if (player2.turn == "true" && tiles[i].className == "tile") {
+            tiles[i].className = "o"; 
+            player2.turn = "false";
+            player1.turn = "true";
+            turns += 1;
+        };
+        //checkWin();
+    });
+};
 
-    const checkTurns = () => {
-        if (turns == 3) {
-            for (i = 0; i < tile.length; i++) {
-                tile[i].innerHTML = "";
-            }
-        }
-    };
+    // const checkWin = () => {
+
+    //     if (tile1 == Tiles[1] && Tiles[0] == Tiles[2]) {
+    //         console.log("test");
+    //     }
+
+    // };
+
+    // const checkTurns = () => {
+    //     if (turns == 3) {
+    //         for (i = 0; i < tile.length; i++) {
+    //             tile[i].innerHTML = "";
+    //         }
+    //     }
+    // };
     
     //const checkWin
 
-    return {
-        makeColumns,
-        makeTiles,
-        checkTurns
-    };
+    // return {
+    //     checkWin,
+    //     checkTurns
+    // };
 })();
 
-makeBoard.makeColumns();
-makeBoard.makeTiles();
-makeBoard.checkTurns();
+// makeBoard.checkWin();
+// makeBoard.checkTurns();
+
 
 
 // (column0[i] = column1[i] = column2[i])
