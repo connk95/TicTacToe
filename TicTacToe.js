67d7194ff9149c50f8180e179639d69cbd930a23
Connor Ketcheson <connorketcheson01@gmail.com>
2022-12-15 09:58:13 +0900

    modify winCheck function

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,16 +1,14 @@
 const makeBoard = (() => {
 
-    const createPlayer = (player, sign, turn, ai) => {
-        return {player, sign, turn, ai}
+    const createPlayer = (player, sign, turn, ai, selectedTiles) => {
+        return {player, sign, turn, ai, selectedTiles}
     };
     
-    const player1 = createPlayer("1", "x", "true", "false");
-    const player2 = createPlayer("2", "o", "false", "false");
+    const player1 = createPlayer("1", "x", "true", "false", []);
+    const player2 = createPlayer("2", "o", "false", "false", []);
 
     let turns = 0;
     const gameBoard = [];
-    let xBoard = [];
-    let oBoard = [];
     const tiles = document.getElementsByClassName("tile"); 
     for (i = 0; i < tiles.length; i++) {  
         let thisTile = document.getElementById(`${i}`);
@@ -21,42 +19,58 @@ const makeBoard = (() => {
                 player2.turn = "true";
                 turns += 1;
                 gameBoard.push(thisTile.id);
-                xBoard.push(thisTile.id);
+                player1.selectedTiles.push(thisTile.id);
+                winCheck(player1.selectedTiles);
             } else if (player2.turn == "true" && gameBoard.includes(thisTile.id) == false) {
                 thisTile.innerHTML = player2.sign; 
                 player2.turn = "false";
                 player1.turn = "true";
                 turns += 1;
                 gameBoard.push(thisTile.id);
-                oBoard.push(thisTile.id);
+                player2.selectedTiles.push(thisTile.id);
+                winCheck(player2.selectedTiles);
             };
-            winCheck();
+            console.log(winCheck(player1.selectedTiles));
+            console.log(winCheck(player2.selectedTiles));
         });
     };
 
-    const wins = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
+    const winCheck = (selectedTiles) => {
 
-    winCheck = () => {
+        const output = false;
+
+        const wins = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
         for (let i = 0; i < wins.length; i++) {
             const winFormula = wins[i];
-            let xWin = winFormula.every((win) => xBoard.includes(win));
-            let oWin = winFormula.every((win) => oBoard.includes(win));
+            const winTally = 0;
 
-            if (xWin || oWin) {
-                return xWin ? "x" : "o";
+            for (let j = 0; j < winFormula.length; j++) {
+                const eachSpot = winFormula[j];
+
+                if (selectedTiles.includes(eachSpot)) {
+                    winTally++;
+
+                    if (winTally === 3) {
+                        output = true;
+                    };
+                };
             };
-            console.log(xWin);
         };
+
+        return output;
     };
+    winCheck(player1.selectedTiles);
+    console.log(winCheck(player1.selectedTiles));
 
 })();
 
